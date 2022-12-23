8ba8150f69cd196a2f02989219bc7d41596df454
rumon5h <mdrumonkhan48519@gmail.com>
2022-12-23 18:59:18 +0600

    post route

--- server.js
+++ server.js
@@ -8,3 +8,14 @@ dotenv.config();
 const configuration = new Configuration({
   apiKey: process.env.OPEN_AI_API_KEY,
 });
+
+const openAI = new OpenAIApi(configuration);
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res, next) => {
+  res.status(200).send({
+    message: "Hello, World!",
+  });
+});
