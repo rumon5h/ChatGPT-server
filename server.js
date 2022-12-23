bd6644fea72a0cc058cdb7ab4a82fba318d4997f
rumon5h <mdrumonkhan48519@gmail.com>
2022-12-23 21:01:11 +0600

    done

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 const app = express();
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.status(200).send({
@@ -27,20 +27,19 @@ app.post("/", async (req, res) => {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: `${prompt}`,
-      temperature: 0,
-      max_tokens: 3000,
+      temperature: 0.7,
+      max_tokens: 64,
       top_p: 1,
-      frequency_penalty: 0.5,
-      presence_penalty: 0
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      stop: ['"""'],
     });
-    
     res.status(200).send({
-        bot: response.data.choices[0].text
-    })
+      bot: response.data.choices[0].text,
+    });
   } catch (error) {
-    res.status(500).send({error})
+    res.status(500).send({ error });
   }
 });
 
-
-app.listen(PORT, () => console.log('Server is running on port', PORT));
+app.listen(PORT, () => console.log("Server is running on port", PORT));
