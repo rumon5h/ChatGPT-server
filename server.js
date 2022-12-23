1a19e387677bd2d94ad5ac79970c33122590be58
rumon5h <mdrumonkhan48519@gmail.com>
2022-12-23 19:00:48 +0600

    done

--- server.js
+++ server.js
@@ -19,3 +19,28 @@ app.get("/", (req, res, next) => {
     message: "Hello, World!",
   });
 });
+
+app.post("/", async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+
+    const response = await openAI.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 0,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0
+    });
+    
+    res.status(200).send({
+        bot: response.data.choices[0].text
+    })
+  } catch (error) {
+    res.status(500).send({error})
+  }
+});
+
+
+app.listen(5000, () => console.log('Server is running on port 5000'));
